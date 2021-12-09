ea5d8982480d18f9707480a3a8df00ba9c36a2cb
sammohara <samuel@mohara.co>
2021-12-09 20:31:53 +0200

    1 file modified

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-// for transpiling all ESM @fullcalendar/* packages
-// also, for piping fullcalendar thru babel (to learn why, see babel.config.js)
 const withTM = require('next-transpile-modules')(['@fullcalendar']);
 
 module.exports = withTM({
